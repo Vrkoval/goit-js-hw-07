1f5cd68121161ad8eda4d2499cbed47c14105723
Viktoriya Koval <vrkoval@gmail.com>
2023-03-28 20:37:21 +0300

    js galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryElem=document.querySelector(".gallery");
+function createGalleryItem(items) {
+    return items
+        .map(({preview,original,description})=>{
+            return `<div class="gallery__item">
+                        <a class="gallery__link" href="#"
+                            <img class="gallery__image"
+                            src="${preview}"
+                            data-original="${original}"
+                            alt="${description}">
+                        </a>
+                    </div>`;
+        })
+        .join("");
+        const addGalleryItem = createGalleryItem(galleryItems);
+        galleryElem.insertAdjacentHTML("beforeend", addGalleryItem);
+        
+        galleryElem.addEventListener("click", onImageClick);
+        
+        function onImageClick(e) {
+          blockDefaultSettings(e);
+        
+          checkImageClass(e);
+        
+          const instance =
+            basicLightbox.create(`<img src="${e.target.dataset.original}" 
+          alt="${e.target.alt}" width="800" height="600">`);
+          instance.show();
+          galleryElem.addEventListener("keydown", (e) => {
+            if (e.code === "Escape") {
+              instance.close();
+            }
+          });
+        }
+        
+        const blockDefaultSettings = (e) => {
+          e.preventDefault();
+        };
+        const checkImageClass = (e) => {
+          if (!e.target.classList.contains("gallery__image")) {
+            return;
+          }
+        };
