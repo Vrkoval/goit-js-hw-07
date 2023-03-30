fee4ecaf0e51e5271425d2151488cb941c048a91
Viktoriya Koval <vrkoval@gmail.com>
2023-03-30 13:13:38 +0300

    caption

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -20,3 +20,10 @@ function createGalleryItem(items) {
         const addGalleryItem = createGalleryItem(galleryItems);
         galleryElem.insertAdjacentHTML("beforeend", addGalleryItem);
         
+var lightbox= new SimpleLightbox(".gallery .gallery__link",{
+    captionPosition: "bottom",
+    captionData:"alt",
+    captionDelay:250,
+    close:false,
+    enableKeyboard:true,
+});
