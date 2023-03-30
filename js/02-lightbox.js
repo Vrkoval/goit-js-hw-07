8934ac64c689fbd0b15b09056028dc998fd491a3
Viktoriya Koval <vrkoval@gmail.com>
2023-03-30 13:02:02 +0300

    lightbox2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryElem=document.querySelector(".gallery");
+function createGalleryItem(items) {
+    return items
+        .map(({preview,original,description})=>{
+            return `<div class="gallery__item">
+                        <a class="gallery__link" href="#">
+                            <img class="gallery__image"
+                            src="${preview}"
+                            data-original="${original}"
+                            alt="${description}">
+                        </a>
+                    </div>`;
+        })
+        .join("");
+    }
+        const addGalleryItem = createGalleryItem(galleryItems);
+        galleryElem.insertAdjacentHTML("beforeend", addGalleryItem);
+        
